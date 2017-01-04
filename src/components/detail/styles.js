95b4f8497d0b328eedb188c47e873dc127a5c7bc
Khushboo Gupta <khushboo.gupta@nuvoex.com>
2017-01-04 16:21:33 +0530

    create tracking components and styles
    . made components and distinguish their styles
    . set global value in one place

---
+++ src/components/detail/styles.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Khushi on 04/01/17.
+ */
+import {StyleSheet} from 'react-native';
+import * as GLOBAL from '../utils/Globals';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    center: {
+        alignItems: 'center'
+    },
+    toolbar: {
+        backgroundColor: GLOBAL.COLOR.PRIMARY,
+        height: GLOBAL.SIZE.TOOLBAR_HT
+    },
+    editTextField: {
+        margin: 8
+    },
+    historyContainer: {
+        flexDirection: 'row'
+    },
+    lineCol: {
+        flexDirection: 'column'
+    },
+    ShipmentContainer: {
+        marginTop: GLOBAL.SIZE.DEFAULT_MARGIN
+    },
+    shipmentDetailCol: {
+        flex: 1,
+        paddingLeft: 8,
+        paddingBottom: 12
+    },
+    feature: {
+        flexDirection: 'row',
+        paddingBottom: 8,
+    },
+    historyItem: {
+        flexDirection: 'row',
+        paddingBottom: 4,
+    },
+    heading: {
+        marginLeft: 8,
+        marginTop: 10,
+        fontSize: 12,
+        fontWeight: '500'
+    },
+    bold: {
+        flex: 0.7
+    },
+    historyCard: {
+        paddingTop: 8
+    },
+    label: {
+        flex: 0.3,
+        fontWeight: 'bold'
+    },
+    circle: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: 'transparent',
+        borderColor: 'grey',
+        borderWidth: 2
+    },
+    line: {
+        backgroundColor: 'grey',
+        width: 1,
+        flex: 1,
+        marginLeft: 4,
+        marginTop: 7,
+        marginBottom: 4
+    },
+    blankTopLine: {
+        backgroundColor: GLOBAL.COLOR.WHITE,
+        width: 1,
+        height: 5,
+        marginLeft: 4
+    },
+    errorMsg: {
+        color: GLOBAL.COLOR.RED,
+        padding: GLOBAL.SIZE.STANDARD_PADDING
+    }
+})
---
+++ src/components/utils/Globals.js
@@ -0,0 +1,18 @@
+// @flow
+
+module.exports = {
+    COLOR: {
+        WHITE: '#FFFFFF',
+        GREEN: 'green',
+        RED: '#FF0000',
+        PRIMARY: '#0F303E'
+    },
+    STRINGS: {
+        TRACK_SHIPMENT: 'Track Shipment',
+    },
+    SIZE: {
+        DEFAULT_MARGIN: 16,
+        STANDARD_PADDING: 16,
+        TOOLBAR_HT: 56,
+    }
+};
